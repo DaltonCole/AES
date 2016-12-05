fe0366aea72ecc162c09e3c8ec34aa8a498d4f45
Dalton Cole <dalton_cl@yahoo.com>
2016-12-05 00:08:08 -0600

    Updated driver

--- driver.py
+++ driver.py
@@ -3,28 +3,44 @@ import codecs
 
 
 def main():
-	input_file = 'button.mp3'
-	key = 'abcdefghijklmnopabcdefgh'
-	iv = 'abcdefghijklmnop'
-
-	print(len(key))
-
+	input_file = input("What file would you like to encrypt?: ")
 	file = ''
 	with open(input_file, 'rb') as f:
 		file = f.read()
 
+	mode = ""
+	while mode != 'ECB' and mode != 'CBC':
+		mode = input("Please enter a mode (ECB, CBC): ")
+		if mode != 'ECB' and mode != 'CBC':
+			print("Invalid input")
+
+	key = ""
+	while len(key) != 16 and len(key) != 24 and len(key) != 32:
+		key = input("Inputs a 16, 24, or 32 bit key: ")
+		if len(key) != 16 and len(key) != 24 and len(key) != 32:
+			print("Invalid key size of: " + str(len(key)))
+
+	iv = None
+	if mode == 'CBC':
+		iv = ''
+		while len(iv) != 16:
+			iv = input("Please enter a 16 bit iv")
+			if len(iv) != 16:
+				print("Invalid IV size of: " + str(len(iv)))
+
 	# AES
 	aes = AES(key, iv)
-	e = aes.encrypt(file, 'CBC')	
+	e = aes.encrypt(file, mode)	
+	d = aes.decrypt(e, mode)
 
-	d = aes.decrypt(e, 'CBC')
 
-	with open(str(input_file) + '.aes', 'wb') as f:
+	# Save cipher and plain text to files
+	with open('encryped.aes', 'wb') as f:
 		f.write(e)
-
 	with open('decrypted.mp3', 'wb') as f:
 		f.write(d)
 
+	print("Original equals decyrpted: ")
 	print(d == file)
 
 if __name__ == "__main__":
